6d08db8b8a69ac2c7cea65084ca01dbaea70eed7
bArtOzzzz <breakyourbrain55@gmail.com>
2021-12-13 22:29:25 +0300

    Version 0.0.31
    Добавлены новые функции и команды. Добавлен exe файл с исходным кодом к нему.

---
+++ OriAssistent/Ori Assistent/Commands.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ori_Assistent
+{
+    public class Commands
+    {
+        /// <summary>
+        /// Command 1 | Hello
+        /// </summary>
+        public static List<string> command1 = new List<string>
+        {
+            "Hi", "glad to see you",
+        };
+
+        /// <summary>
+        /// Command 2 | How are you
+        /// </summary>
+        public static List<string> command2 = new List<string>
+        {
+            "I am working normally", "And how do I know"
+        };
+
+        /// <summary>
+        /// Command 3 | What time is it
+        /// </summary>
+        public static List<string> command3 = new List<string>
+        {
+            $"{DateTime.Now: h mm tt yy}"
+        };
+
+        /// <summary>
+        /// Command 4 | Stop talking
+        /// </summary>
+        public static List<string> command4 = new List<string>
+        {
+            "OK", "Fine, I will be quite"
+        };
+
+        /// <summary>
+        /// Command 5 | Stop listening
+        /// </summary>
+        public static List<string> command5 = new List<string>
+        {
+            "if you need me just ask"
+        };
+
+        /// <summary>
+        /// Command 6 | Thank you
+        /// </summary>
+        public static List<string> command6 = new List<string>
+        {
+            "No problems", "I am glad to help you", "You will have to"
+        };
+
+        /// <summary>
+        /// Command 7 | Cancel
+        /// </summary>
+        public static List<string> command7 = new List<string>
+        {
+            "Cancellation successful", "Canceling the operation", "Canceled successfully", "Operation was cancelled"
+        };
+
+        /// <summary>
+        /// Command 8 | Exceptions logo
+        /// </summary>
+        public static List<string> command8 = new List<string>
+        {
+            "Operation cannot be performed", "Unable to execute command"
+        };
+
+        /// <summary>
+        /// Command 9 | Key word - Ori
+        /// </summary>
+        public static List<string> command9 = new List<string>
+        {
+            "I'm here", "I'm waiting for commands", "What do we do"
+        };
+
+        /// <summary>
+        /// Command 10 | OK
+        /// </summary>
+        public static List<string> command10 = new List<string>
+        {
+            "Ok", "No problems", "I will do"
+        };
+    }
+}
---
+++ OriAssistent/Ori Assistent/Ori.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Ori_Assistent
+{
+    partial class OriAssistentStartForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OriAssistentStartForm));
+            this.LeftSideBar = new System.Windows.Forms.Panel();
+            this.SettingsLabelText = new System.Windows.Forms.Label();
+            this.IconImageBox = new System.Windows.Forms.PictureBox();
+            this.GreetingsLabel = new System.Windows.Forms.Label();
+            this.SystemMessageListBox = new System.Windows.Forms.ListBox();
+            this.Settings = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TimerForShotDown = new System.Windows.Forms.TextBox();
+            this.isAutoRunCheckBox = new System.Windows.Forms.CheckBox();
+            this.CommandsList = new System.Windows.Forms.ListBox();
+            this.TimerSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.CurrentDateTime = new System.Windows.Forms.Label();
+            this.CurrentDate = new System.Windows.Forms.Timer(this.components);
+            this.LeftSideBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconImageBox)).BeginInit();
+            this.Settings.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LeftSideBar
+            // 
+            this.LeftSideBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(101)))), ((int)(((byte)(242)))));
+            this.LeftSideBar.Controls.Add(this.SettingsLabelText);
+            this.LeftSideBar.Controls.Add(this.IconImageBox);
+            this.LeftSideBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftSideBar.Location = new System.Drawing.Point(0, 0);
+            this.LeftSideBar.Name = "LeftSideBar";
+            this.LeftSideBar.Size = new System.Drawing.Size(200, 450);
+            this.LeftSideBar.TabIndex = 0;
+            // 
+            // SettingsLabelText
+            // 
+            this.SettingsLabelText.AutoSize = true;
+            this.SettingsLabelText.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SettingsLabelText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SettingsLabelText.ForeColor = System.Drawing.Color.White;
+            this.SettingsLabelText.Location = new System.Drawing.Point(59, 132);
+            this.SettingsLabelText.Name = "SettingsLabelText";
+            this.SettingsLabelText.Size = new System.Drawing.Size(68, 20);
+            this.SettingsLabelText.TabIndex = 1;
+            this.SettingsLabelText.Text = "Settings";
+            this.SettingsLabelText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SettingsLabelText.Click += new System.EventHandler(this.Label1_Click);
+            this.SettingsLabelText.MouseLeave += new System.EventHandler(this.SettingsLabelText_MouseLeave);
+            this.SettingsLabelText.MouseHover += new System.EventHandler(this.Label1_MouseHover);
+            // 
+            // IconImageBox
+            // 
+            this.IconImageBox.Image = ((System.Drawing.Image)(resources.GetObject("IconImageBox.Image")));
+            this.IconImageBox.Location = new System.Drawing.Point(23, 12);
+            this.IconImageBox.Name = "IconImageBox";
+            this.IconImageBox.Size = new System.Drawing.Size(151, 85);
+            this.IconImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.IconImageBox.TabIndex = 0;
+            this.IconImageBox.TabStop = false;
+            // 
+            // GreetingsLabel
+            // 
+            this.GreetingsLabel.AutoSize = true;
+            this.GreetingsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.GreetingsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GreetingsLabel.Location = new System.Drawing.Point(206, 9);
+            this.GreetingsLabel.Name = "GreetingsLabel";
+            this.GreetingsLabel.Size = new System.Drawing.Size(278, 42);
+            this.GreetingsLabel.TabIndex = 1;
+            this.GreetingsLabel.Text = "Good afternoon";
+            this.GreetingsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SystemMessageListBox
+            // 
+            this.SystemMessageListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(0)))), ((int)(((byte)(119)))));
+            this.SystemMessageListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SystemMessageListBox.CausesValidation = false;
+            this.SystemMessageListBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.SystemMessageListBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.SystemMessageListBox.Enabled = false;
+            this.SystemMessageListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.SystemMessageListBox.ForeColor = System.Drawing.Color.White;
+            this.SystemMessageListBox.FormattingEnabled = true;
+            this.SystemMessageListBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.SystemMessageListBox.ItemHeight = 25;
+            this.SystemMessageListBox.Location = new System.Drawing.Point(652, 0);
+            this.SystemMessageListBox.Name = "SystemMessageListBox";
+            this.SystemMessageListBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.SystemMessageListBox.Size = new System.Drawing.Size(148, 450);
+            this.SystemMessageListBox.TabIndex = 2;
+            this.SystemMessageListBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.SystemMessageListBox_DrawItem);
+            this.SystemMessageListBox.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.SystemMessageListBox_MeasureItem);
+            // 
+            // Settings
+            // 
+            this.Settings.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Settings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Settings.Controls.Add(this.label2);
+            this.Settings.Controls.Add(this.TimerForShotDown);
+            this.Settings.Controls.Add(this.isAutoRunCheckBox);
+            this.Settings.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Settings.Location = new System.Drawing.Point(294, 150);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(261, 178);
+            this.Settings.TabIndex = 7;
+            this.Settings.TabStop = false;
+            this.Settings.Text = "Settings";
+            this.Settings.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(44, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Shot down timer";
+            // 
+            // TimerForShotDown
+            // 
+            this.TimerForShotDown.Location = new System.Drawing.Point(12, 66);
+            this.TimerForShotDown.Name = "TimerForShotDown";
+            this.TimerForShotDown.Size = new System.Drawing.Size(26, 26);
+            this.TimerForShotDown.TabIndex = 3;
+            this.TimerForShotDown.Text = "5";
+            this.TimerForShotDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TimerForShotDown.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TimerForShotDown_KeyPress);
+            // 
+            // isAutoRunCheckBox
+            // 
+            this.isAutoRunCheckBox.AutoSize = true;
+            this.isAutoRunCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.isAutoRunCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.isAutoRunCheckBox.Location = new System.Drawing.Point(21, 38);
+            this.isAutoRunCheckBox.Name = "isAutoRunCheckBox";
+            this.isAutoRunCheckBox.Size = new System.Drawing.Size(132, 20);
+            this.isAutoRunCheckBox.TabIndex = 2;
+            this.isAutoRunCheckBox.Text = "Start with windows";
+            this.isAutoRunCheckBox.UseVisualStyleBackColor = true;
+            this.isAutoRunCheckBox.CheckedChanged += new System.EventHandler(this.IsAutoRunCheckBox_CheckedChanged);
+            // 
+            // CommandsList
+            // 
+            this.CommandsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CommandsList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(57)))), ((int)(((byte)(211)))));
+            this.CommandsList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.CommandsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CommandsList.ForeColor = System.Drawing.Color.White;
+            this.CommandsList.FormattingEnabled = true;
+            this.CommandsList.ItemHeight = 20;
+            this.CommandsList.Location = new System.Drawing.Point(222, 97);
+            this.CommandsList.Name = "CommandsList";
+            this.CommandsList.Size = new System.Drawing.Size(408, 320);
+            this.CommandsList.TabIndex = 6;
+            this.CommandsList.Visible = false;
+            // 
+            // TimerSpeaking
+            // 
+            this.TimerSpeaking.Enabled = true;
+            this.TimerSpeaking.Interval = 1000;
+            this.TimerSpeaking.Tick += new System.EventHandler(this.TimerSpeaking_Tick);
+            // 
+            // CurrentDateTime
+            // 
+            this.CurrentDateTime.AutoSize = true;
+            this.CurrentDateTime.BackColor = System.Drawing.Color.Transparent;
+            this.CurrentDateTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.CurrentDateTime.Location = new System.Drawing.Point(210, 51);
+            this.CurrentDateTime.Name = "CurrentDateTime";
+            this.CurrentDateTime.Size = new System.Drawing.Size(67, 16);
+            this.CurrentDateTime.TabIndex = 9;
+            this.CurrentDateTime.Text = "DateTime";
+            // 
+            // CurrentDate
+            // 
+            this.CurrentDate.Enabled = true;
+            this.CurrentDate.Interval = 1000;
+            this.CurrentDate.Tick += new System.EventHandler(this.CurrentDate_Tick);
+            // 
+            // OriAssistentStartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CurrentDateTime);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.CommandsList);
+            this.Controls.Add(this.GreetingsLabel);
+            this.Controls.Add(this.LeftSideBar);
+            this.Controls.Add(this.SystemMessageListBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OriAssistentStartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ori assistent";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OriAssistentStartForm_FormClosing);
+            this.Load += new System.EventHandler(this.OriAssistentStartForm_Load);
+            this.Click += new System.EventHandler(this.OriAssistentStartForm_Click);
+            this.LeftSideBar.ResumeLayout(false);
+            this.LeftSideBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconImageBox)).EndInit();
+            this.Settings.ResumeLayout(false);
+            this.Settings.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel LeftSideBar;
+        private System.Windows.Forms.PictureBox IconImageBox;
+        private System.Windows.Forms.Label SettingsLabelText;
+        private System.Windows.Forms.ListBox SystemMessageListBox;
+        private System.Windows.Forms.GroupBox Settings;
+        private System.Windows.Forms.CheckBox isAutoRunCheckBox;
+        public System.Windows.Forms.ListBox CommandsList;
+        private System.Windows.Forms.Timer TimerSpeaking;
+        private System.Windows.Forms.TextBox TimerForShotDown;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label GreetingsLabel;
+        private System.Windows.Forms.Label CurrentDateTime;
+        private System.Windows.Forms.Timer CurrentDate;
+    }
+}
+
